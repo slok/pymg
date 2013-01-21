9989414d5c366b055d4b4d3382753502c0b2e197
Xabier Larrakoetxea <slok69@gmail.com>
2013-01-21 22:17:22 +0100

    Implement the view for the square

--- pymg/imageserver/views.py
+++ pymg/imageserver/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import (render_to_response,
                              RequestContext)
+from django.http import HttpResponse
+
+from imagecreator import utils
+from imagecreator.draw import Square
 
 
 def index(request):
@@ -11,7 +15,13 @@ def index(request):
 
 
 def draw_width_height(request, width, height):
-    print(width)
-    print(height)
-    return render_to_response('imageserver/index.html',
-                            context_instance=RequestContext(request))
+    #Create the image
+    width, height = int(width), int(height)
+    color = utils.random_color()
+    s = Square(width, height, color)
+    s.draw()
+
+    #Get the content of the image in memory
+    data = s.get_img_data()
+
+    return HttpResponse(data, content_type="image/png")
