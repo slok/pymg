5926a547d4c8cd8c39b251fb6007f3612965e98c
Xabier Larrakoetxea <slok69@gmail.com>
2013-01-21 21:35:47 +0100

    Added first url for the simple creation

--- pymg/imageserver/urls.py
+++ pymg/imageserver/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import patterns, url
 import imageserver.views
 
 urlpatterns = patterns('',
+    # /100x200
+    url(r'(?P<width>\d+)x(?P<height>\d+)$', imageserver.views.draw_width_height,
+                                            name="draw_width_height"),
+    # /
     url(r'$', imageserver.views.index, name="index"),
 )
--- pymg/imageserver/views.py
+++ pymg/imageserver/views.py
@@ -8,3 +8,10 @@ def index(request):
     return render_to_response('imageserver/index.html',
                             data,
                             context_instance=RequestContext(request))
+
+
+def draw_width_height(request, width, height):
+    print(width)
+    print(height)
+    return render_to_response('imageserver/index.html',
+                            context_instance=RequestContext(request))
