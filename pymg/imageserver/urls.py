9d486cfe8312994db57c029215dfcabb3a19744e
Xabier Larrakoetxea <slok69@gmail.com>
2013-01-22 20:42:28 +0100

    Edit urls

--- pymg/imageserver/urls.py
+++ pymg/imageserver/urls.py
@@ -4,16 +4,18 @@ import imageserver.views
 
 urlpatterns = patterns('',
 
-    # /c/100x200
-    url(r'c/(?P<radius>\d+)$',
-        imageserver.views.draw_circle_width_height,
-        name="draw_circle_width_height"),
+    # /
+    url(r'^$', imageserver.views.index, name="index"),
 
-    # /100x200
-    url(r'(?P<width>\d+)x(?P<height>\d+)$',
+    # /100x200 or /s/100x200
+    url(r'^(s/)?(?P<width>\d+)x(?P<height>\d+)$',
         imageserver.views.draw_square_width_height,
         name="draw_square_width_height"),
 
-    # /
-    url(r'$', imageserver.views.index, name="index"),
+    # /c/100x200
+    url(r'^c/(?P<radius>\d+)$',
+        imageserver.views.draw_circle_width_height,
+        name="draw_circle_width_height"),
+
+
 )
